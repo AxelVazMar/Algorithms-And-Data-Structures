1451101f2ebf75e9c7632d060a1ea9af4a3f1cc6
Axel Vazquez <axelvazmar0@gmail.com>
2024-12-07 10:39:21 -0600

    Starting a course about Algorithms and Data Structures

---
+++ LogsAllPairs/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < arr.Length; i++)
+{
+    int aux = arr[i];
+    for (int j = 0; j < arr.Length; j++)
+    {
+        int aux2 = arr[j];
+        Console.WriteLine(aux + "," + aux2);
+    }
+}
+
+// Big O of the algorithm to show all pairs = O(n(1st foor loop) * n (2do for loop)) lo que sería = O(n^2)
